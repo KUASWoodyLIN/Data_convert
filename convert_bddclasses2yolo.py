0f3ec12230de80d285cb134f651434ea072d8c95
KUASWoodyLin <jj7158761@gmail.com>
2018-08-15 10:37:00 +0800

    fix convert bdd2yolo classes

--- convert_bddclasses2yolo.py
+++ convert_bddclasses2yolo.py
@@ -3,10 +3,15 @@ import json
 from collections import OrderedDict
 
 # TODO: Change Path Name Here
-train_day = 'day'
-train_night = 'night'
-val_day = 'day'
-val_night = 'night'
+train_day = 'day_city_street100'
+train_night = 'night_city_street100'
+val_day = 'day_city_street8'
+val_night = 'night_city_street8'
+
+#train_day = 'day'
+#train_night = 'night'
+#val_day = 'day'
+#val_night = 'night'
 objects = {'person'}
 
 ROOT_PATH = os.path.split(os.getcwd())[0]
@@ -23,14 +28,14 @@ SAVE_TRAIN_DAY_LABELS_FILE = os.path.join(DATASET_PATH, 'train_' + train_day + '
 SAVE_TRAIN_NIGHT_LABELS_FILE = os.path.join(DATASET_PATH, 'train_' + train_night + '.txt')
 
 # Validation path
-VAL_DAY_IMAGES_PATH = os.path.join(IMAGES_PATH, 'val/' + val_day)
-VAL_DAY_LABELS_PATH = os.path.join(LABELS_PATH, 'val')
-VAL_NIGHT_IMAGES_PATH = os.path.join(IMAGES_PATH, 'val/' + val_night)
-VAL_NIGHT_LABELS_PATH = os.path.join(LABELS_PATH, 'val')
 # VAL_DAY_IMAGES_PATH = os.path.join(IMAGES_PATH, 'val/' + val_day)
-# VAL_DAY_LABELS_PATH = os.path.join(LABELS_PATH, 'train')
+# VAL_DAY_LABELS_PATH = os.path.join(LABELS_PATH, 'val')
 # VAL_NIGHT_IMAGES_PATH = os.path.join(IMAGES_PATH, 'val/' + val_night)
-# VAL_NIGHT_LABELS_PATH = os.path.join(LABELS_PATH, 'train')
+# VAL_NIGHT_LABELS_PATH = os.path.join(LABELS_PATH, 'val')
+VAL_DAY_IMAGES_PATH = os.path.join(IMAGES_PATH, 'val/' + val_day)
+VAL_DAY_LABELS_PATH = os.path.join(LABELS_PATH, 'train')
+VAL_NIGHT_IMAGES_PATH = os.path.join(IMAGES_PATH, 'val/' + val_night)
+VAL_NIGHT_LABELS_PATH = os.path.join(LABELS_PATH, 'train')
 SAVE_VAL_DAY_LABELS_FILE = os.path.join(DATASET_PATH, 'val_' + val_day + '.txt')
 SAVE_VAL_NIGHT_LABELS_FILE = os.path.join(DATASET_PATH, 'val_' + val_night + '.txt')
 
@@ -59,7 +64,7 @@ def convert_bdd2yolo(images_file, read_images_path, read_labels_path, save_file)
                         top = int(object['box2d']['y1'])
                         right = int(object['box2d']['x2'])
                         bottom = int(object['box2d']['y2'])
-                        class_id = 0
+                        class_id = 0 # cls[obj_name]
                         txt += (" " + str(left) + ',' + str(top) + ',' + str(right) + ',' + str(bottom) + ',' + str(class_id))
                     total_name[obj_name] = total_name.setdefault(obj_name, 0) + 1
         if txt:
